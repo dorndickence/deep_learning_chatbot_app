7de4b0f7e37471500267d067c5be4d741f66509d
Dornavan <130479724+dorndickence@users.noreply.github.com>
2024-02-13 17:00:04 +0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -19,39 +19,32 @@ ignore_words = ['?', '!']
 data_file = open('intents.json', encoding='utf-8').read()
 intents = json.loads(data_file)
 
-
 for intent in intents['intents']:
     for pattern in intent['patterns']:
-
         w = nltk.word_tokenize(pattern)
         words.extend(w)
-
         documents.append((w, intent['tag']))
-
 
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
 words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
 words = sorted(list(set(words)))
-
 classes = sorted(list(set(classes)))
 
-print (len(documents), "documents")
+print(len(documents), "documents")
+print(len(classes), "classes", classes)
+print(len(words), "unique lemmatized words", words)
 
-print (len(classes), "classes", classes)
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
 
-print (len(words), "unique lemmatized words", words)
+# Initializing training data
+max_len = len(max(documents, key=lambda x: len(x[0]))[0])
 
-
-pickle.dump(words,open('words.pkl','wb'))
-pickle.dump(classes,open('classes.pkl','wb'))
-
-# initializing training data
 training = []
 output_empty = [0] * len(classes)
 for doc in documents:
-
     bag = []
 
     pattern_words = doc[0]
@@ -60,6 +53,9 @@ for doc in documents:
     for w in words:
         bag.append(1) if w in pattern_words else bag.append(0)
 
+    # Pad the bag with zeros if its length is less than the maximum length
+    if len(bag) < max_len:
+        bag += [0] * (max_len - len(bag))
 
     output_row = list(output_empty)
     output_row[classes.index(doc[1])] = 1
@@ -68,27 +64,26 @@ for doc in documents:
 
 random.shuffle(training)
 training = np.array(training)
-# create train and test lists. X - patterns, Y - intents
-train_x = list(training[:,0])
-train_y = list(training[:,1])
+
+# Create train and test lists. X - patterns, Y - intents
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
 print("Training data created")
 
-
-# Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
-# equal to number of intents to predict output intent with softmax
+# Create model
 model = Sequential()
-model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dense(128, input_shape=(max_len,), activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
-# Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
+# Compile model
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-#fitting and saving the model
+# Fit and save the model
 hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
 model.save('chatbot_model.h5', hist)
 
-print("model created")
+print("Model created")
